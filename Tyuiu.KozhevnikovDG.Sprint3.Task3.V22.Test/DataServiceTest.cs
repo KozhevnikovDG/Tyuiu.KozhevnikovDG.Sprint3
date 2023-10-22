d3ff5a9f040388de15e2987e6dbb612e7b614f6d
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-10-22 23:18:46 +0500

    Task3

---
+++ Tyuiu.KozhevnikovDG.Sprint3.Task3.V22.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KozhevnikovDG.Sprint3.Task3.V22.Lib;
+
+namespace Tyuiu.KozhevnikovDG.Sprint3.Task3.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "tbtbbb dsfbg bbg";
+
+            char chr = 'b';
+
+            double res = ds.GetMaxCharCount(str, chr);
+            int wait = 7;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovDG.Sprint3.Task3.V22/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozhevnikovDG.Sprint3.Task3.V22.Lib;
+
+namespace Tyuiu.KozhevnikovDG.Sprint3.Task3.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Кожевников Д. Г. | ИИПБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла forech                                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Кожевников Данил Георгиевич | ИИПБ-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, используя цикл foreach, подсчитать максимальное кол-во*");
+            Console.WriteLine("* букв d, находяшихся на соседних позициях в строке                       *");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "tbtbbb dsfbg bbg";
+
+            char chr = 'b';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Исходный символ = " + chr);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество символов = " + ds.GetMaxCharCount(value,chr));
+
+            Console.ReadKey();
+        }
+    }
+}
