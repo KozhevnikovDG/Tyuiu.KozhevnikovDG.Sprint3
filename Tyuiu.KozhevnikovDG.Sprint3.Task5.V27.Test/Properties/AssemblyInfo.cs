54db4ae33d9e8b7539ddf6e676670c47d75b7b4e
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-10-29 17:30:25 +0500

    Task5

---
+++ Tyuiu.KozhevnikovDG.Sprint3.Task5.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KozhevnikovDG.Sprint3.Task5.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KozhevnikovDG.Sprint3.Task5.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2629ff01-518b-4753-a859-aba0dcb261b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
